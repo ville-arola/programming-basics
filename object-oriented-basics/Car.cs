6ff2f1389e2dc9e5b4074902a42c3e62beaa91ce
ville-arola <1700383@student.saimia.fi>
2017-02-27 10:15:56 +0200

    Object-oriented basics solution added.

---
+++ object-oriented-basics/Car.cs
@@ -0,0 +1,41 @@
+﻿namespace car
+{
+    public class Car
+    {
+        // Default constructor
+        public Car ()
+        {
+            Brand = "Ford";
+            Speed = 80.0f;
+        }
+        // Constructor with parameters
+        public Car(string brand)
+        {
+            Brand = brand;
+            Speed = 0;
+        }
+
+        // Properties
+        public string Brand { get; } // autoproperty (palauttaa brand-arvon sellaisenaan)
+        public float Speed { get; private set; }
+
+        // Methods
+        public override string ToString()
+        {
+            return Print();
+        }
+        public void Accelerate(float speedGain)
+        {
+            Speed += speedGain;
+        }
+        public void Brake()
+        {
+            Speed *= 0.9f;
+        }
+
+        private string Print()
+        {
+            return string.Format("Merkki: {0}\nNopeus: {1} km/h", Brand, Speed);
+        }
+    }
+}
---
+++ object-oriented-basics/Tester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace car
+{
+    public class Tester
+    {
+        static void Main(string[] args)
+        {
+            var testCar1 = new Car();
+            var testCar2 = new Car("VW");
+            var testCar3 = new Car("Toyota");
+
+            Console.WriteLine("Autot ohjelman alussa:");
+            Console.WriteLine(testCar1);
+            Console.WriteLine(testCar2);
+            Console.WriteLine(testCar3);
+
+            Console.WriteLine("\nTestCar1 kiihdyttää 12 km/h ja jarruttaa.");
+            testCar1.Accelerate(12);
+            testCar1.Brake();
+
+            Console.WriteLine("\nTestCar2 jarruttaa kaksi kertaa ja kiihdyttää 4,3 km/h.");
+            testCar2.Brake();
+            testCar2.Brake();
+            testCar2.Accelerate(4.3f);
+
+            Console.WriteLine("\nTestCar3 kiihdyttää 10 km/h ja jarruttaa.");
+            testCar3.Accelerate(10);
+            testCar3.Brake();
+
+            Console.WriteLine("\nAutot ohjelman lopussa:");
+            Console.WriteLine(testCar1.ToString());
+            Console.WriteLine(testCar2.ToString());
+            Console.WriteLine(testCar3.ToString());
+            Console.ReadKey();
+        }
+    }
+}
